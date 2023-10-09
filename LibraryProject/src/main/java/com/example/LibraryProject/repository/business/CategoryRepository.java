85185cfad1588fb2a6dbeba0879854c8e5c8ec7d
FevziBAYKUS <132695111+FevziBAYKUS@users.noreply.github.com>
2023-10-09 09:03:01 +0300

    Fevzi-Mustafa Entity Class'lar Olusturuldu

--- LibraryProject/src/main/java/com/example/LibraryProject/repository/business/CategoryRepository.java
+++ LibraryProject/src/main/java/com/example/LibraryProject/repository/business/CategoryRepository.java
@@ -1,2 +1,7 @@
-package com.example.LibraryProject.repository.business;public interface CategoryRepository {
+package com.example.LibraryProject.repository.business;
+
+import com.example.LibraryProject.entity.business.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
