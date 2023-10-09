4e93f8385dc97a3fca1afd4d5eeb277714c0e3e8
hasan25hasan <hb25315312@gmail.com>
2023-10-09 13:08:33 +0300

    hasandeneme

--- LibraryProject/src/main/java/com/example/LibraryProject/entity/business/Author.java
+++ LibraryProject/src/main/java/com/example/LibraryProject/entity/business/Author.java
@@ -1,4 +1,17 @@
 package com.example.LibraryProject.entity.business;
 
+
+import lombok.*;
+
+import javax.persistence.Entity;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
 public class Author {
+
 }
