32d11ac3444c65257fe2e89158f2529f2e3e7c1a
Emre Akkaş <akkasemre.0021@gmail.com>
2023-10-11 00:06:43 +0300

    reportCont-service-repo

--- LibraryProject/src/main/java/com/example/LibraryProject/controller/business/ReportController.java
+++ LibraryProject/src/main/java/com/example/LibraryProject/controller/business/ReportController.java
@@ -1,10 +1,10 @@
 package com.example.LibraryProject.controller.business;
 
 import com.example.LibraryProject.payload.business.response.ReportResponse;
+import com.example.LibraryProject.payload.business.response.ResponseMessage;
 import com.example.LibraryProject.service.business.ReportService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RequestMapping("/report")
 public class ReportController {
 
@@ -21,22 +21,14 @@ public class ReportController {
 
 //    @PreAuthorize("hasAnyAuthority('EMPLOYEE','ADMIN')")
     @GetMapping("/reports")
-    public Map<String, Integer> reportAll(){
+    public ResponseMessage<Map<String,Integer>> reportAll(){
         return reportService.reportAll();
     }
 
 
-
-
-
-
-
-
-
-
     //    @PreAuthorize("hasAnyAuthority('EMPLOYEE','ADMIN')")
-    @GetMapping("/most-popular-books")
-    public Page<ReportResponse> mostPopularBooks(
+    @GetMapping("most-popular-books")
+    public Page<ReportResponse>getMostPopularBooks(
             @RequestParam(value = "page",defaultValue = "0") int page,
             @RequestParam(value = "size",defaultValue = "20") int size,
             @RequestParam(value = "sort") String sort,
@@ -44,7 +36,6 @@ public class ReportController {
     ){
         return reportService.getMostPopularBooks(page,size,sort,type);
     }
-
 
 
 
