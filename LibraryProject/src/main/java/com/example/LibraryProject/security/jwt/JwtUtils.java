e309ce9f34e5747f5965d02030614fdd2f8032af
Emre Akkaş <akkasemre.0021@gmail.com>
2023-10-12 14:26:40 +0300

    Jwtutils, AuthEntryPointJwt

--- LibraryProject/src/main/java/com/example/LibraryProject/security/jwt/JwtUtils.java
+++ LibraryProject/src/main/java/com/example/LibraryProject/security/jwt/JwtUtils.java
@@ -26,10 +26,41 @@ public class JwtUtils {
     public String generateJwtToken(Authentication authentication){
         UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
         return generateJwtTokenFromEmail(userDetails.getEmail());
+
     }
 
     public String generateJwtTokenFromEmail(String email){
-
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS256, jwtSecret)
+                .compact();
     }
 
+    public boolean validateJwtToken(String jwtToken){
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(jwtToken);
+            return true;
+        } catch (ExpiredJwtException e) {
+            LOGGER.error("Jwt token is expired : {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            LOGGER.error("Jwt token is unsupported : {}", e.getMessage());
+        } catch (MalformedJwtException e) {
+            LOGGER.error("Jwt token is invalid : {}", e.getMessage());
+        } catch (SignatureException e) {
+            LOGGER.error("Jwt token is invalid : {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            LOGGER.error("Jwt  is empty : {}", e.getMessage());
+        }
+        return false;
+    }
+
+    public String getEmailFromJwtToken(String jwtToken){
+        return Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(jwtToken)
+                .getBody()
+                .getSubject();
+    }
 }
