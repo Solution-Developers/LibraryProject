59e3a4194b14937e340dc4ffceea862955edec8e
Mustafa-Uslu <69405526+Mustafa-Uslu@users.noreply.github.com>
2023-10-15 19:29:06 +0200

    Update BookRequestUpdate.java

--- src/main/java/com/example/LibraryProject/payload/business/request/abstracts/BookRequestUpdate.java
+++ src/main/java/com/example/LibraryProject/payload/business/request/abstracts/BookRequestUpdate.java
@@ -1,2 +1,46 @@
-package com.example.LibraryProject.payload.business.request.abstracts;public class BookRequestUpdate {
+package com.example.LibraryProject.payload.business.request.abstracts;
+
+
+import lombok.*;
+
+
+import javax.validation.constraints.NotNull;
+import java.io.File;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public abstract class BookRequestUpdate {
+
+
+    @NotNull(message = "Please must be write book name")
+    private String name;
+
+    @NotNull(message = "Please must be write book name")
+    private String isbn;
+
+    private int pageCount;
+
+    @NotNull(message = "Please must be write book name")
+    private Long authorId;
+
+    @NotNull(message = "Please must be write book name")
+    private Long publisherId;
+
+    private int publishDate;
+
+    @NotNull(message = "Please must be write book name")
+    private Long categoryId;
+
+    private File image;
+
+    @NotNull(message = "Please must be write book name")
+    private String shelfCode;
+
+    @NotNull(message = "Please must be write book name")
+    private boolean featured;
+
 }
